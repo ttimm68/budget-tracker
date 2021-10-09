16bcd27e79501b6ba545e53b5771c6631c823224
ttimm68 <ttimm16@gmail.com>
2021-10-09 00:00:09 -0500

    Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Budget Tracker!</h1>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div className='App'>
+    <header>
+      <Header />
+    </header>
+  </div>;
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header className='App-header'>
+      <h1>Budget Tracker!</h1>
+    </header>
+  );
+};
